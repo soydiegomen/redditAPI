37108a76413112f96d5a72dbfbc32c626f1ce7da
Diego Mendoza <dmendoza@inflexionsoftware.com>
2018-07-14 22:36:35 -0500

    Complete la funcionalidad del proyecto Reddit API

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -6,15 +6,65 @@ import {
   fetchPostsIfNeeded,
   invalidateSubreddit
 } from '../actions';
-import Posts from '../components/Posts'
+import Posts from '../components/Posts';
+import Picker from '../components/Picker';
 
 //TODO EMPECEMOS POR MOSTRAR LOS POSTS
 class AsyncApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleRefreshClick = this.handleRefreshClick.bind(this);
+  }
+
+  componentDidMount() {
+    const { dispatch, selectedSubreddit } = this.props;
+    dispatch(fetchPostsIfNeeded(selectedSubreddit));
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.selectedSubreddit !== prevProps.selectedSubreddit) {
+      const { dispatch, selectedSubreddit } = this.props;
+      dispatch(fetchPostsIfNeeded(selectedSubreddit));
+    }
+  }
+
+  handleChange(nextSubreddit) {
+    this.props.dispatch(selectSubreddit(nextSubreddit));
+    this.props.dispatch(fetchPostsIfNeeded(nextSubreddit));
+  }
+
+  handleRefreshClick(e) {
+    e.preventDefault();
+    const { dispatch, selectedSubreddit } = this.props;
+    dispatch(invalidateSubreddit(selectedSubreddit));
+    dispatch(fetchPostsIfNeeded(selectedSubreddit));
+  }
+
   render() {
     const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props;
     return (
       <div>
         <h1>Async App</h1>
+        <Picker
+          value={selectedSubreddit}
+          onChange={this.handleChange}
+          options={['reactjs', 'frontend', 'nodejs']}
+        />
+        <p>
+          {lastUpdated &&
+            <span>
+              Last updated at {new Date(lastUpdated).toLocaleTimeString()}.
+              {' '}
+            </span>}
+          {!isFetching &&
+            <button onClick={this.handleRefreshClick}>
+              Refresh
+            </button>}
+        </p>
+        {isFetching && posts.length === 0 && <h2>Loading...</h2>}
+        {!isFetching && posts.length === 0 && <h2>Empty.</h2>}
         {posts.length > 0 &&
           <div style={{ opacity: isFetching ? 0.5 : 1 }}>
             <Posts posts={posts} />
@@ -34,7 +84,7 @@ function mapStateToProps(state) {
     isFetching: true,
     items: []
   };
-  
+
   return {
     selectedSubreddit,
     posts,
